a59130900a97745e1dae0b9639b70f327dffb946
waimea-Jadavidson <jadavidson@waimea.school.nz>
2024-10-22 09:56:30 +1300

    [Commit] Dev
    Implemented New Coin creation system, I have also implemented the player name system in a function

--- src/Main.kt
+++ src/Main.kt
@@ -18,32 +18,77 @@ import kotlin.random.Random
  * Program entry point
  */
 fun main() {
-    initBoard() // Menu entry point for game
+    menu()
+
+    // initBoard(10, 3) // Menu entry point for game
 }
 
 fun menu(){
-
+    println("Welcome to Old Gold")
+    println("")
+    while (true){
+        println("Options:")
+        println("[1]. Play")
+        println("[2]. Exit")
+        print("Select: ")
+        val option = readln()
+        when(option){
+            "1" -> {
+                players()
+                println()
+                break
+            }
+            "2" -> break
+            else -> {
+                println("Invalid Option")
+                println()
+            }
+        }
+    }
 }
 
-fun initBoard(){
-    var response: String? = null
+fun players(){
+    println()
+    var numPlayers: Int
+    val playersList = arrayListOf<String>()
     while (true) {
-        print("Board Size: ")
-        response = readlnOrNull()
-        if (!response.isNullOrEmpty()) {
+        while (true) {
+            print("How many people are playing?: ")
+            numPlayers = readln().toInt()
+            if (numPlayers <=1){
+                println("Invalid Number of Players, please try again")
+            }
+            else{
+                break
+            }
+        }
+        println()
+        print("Are you sure? y/n: ")
+        if (readln().lowercase() == "y") {
+            println()
             break
         }
     }
-    val bLength = response!!.toInt()
-    val coins = bLength/3
-    val gBoard = mutableListOf<String>()
-    for (i in 0..<bLength) {
-        gBoard.add(i, "[O]")
+    while (true) {
+        repeat(numPlayers) { i ->
+            print("What is player ${i + 1} name: ") // +1 required for Zero Index
+            playersList.add(i, readln().toString())
+            println()
+        }
+    }
+}
 
+fun initBoard(bSize: Int, bCoin: Int){  // Function takes in board size and number of coins
+    val board = mutableListOf<String>()
+    println(bSize-(bSize/bCoin))
+    for(i in 1 .. bSize-(bSize/bCoin)){
+        board.add(0, "[_]")
     }
-    for( i in 0 .. coins){
-        val j = (0..gBoard.size).random()
-        gBoard.replace(j, "[C]")
+    println(bSize/bCoin)
+    for(i in 1..<(bSize/bCoin)){
+        board.add(0, "[C]")
     }
-    println(gBoard.joinToString(""))
+    board.add(0, "[G]")
+    board.shuffle()
+    println(board.joinToString(""))
 }
