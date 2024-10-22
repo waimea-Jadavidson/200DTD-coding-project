722322ec36a3a18dbb18b74ed28bd005c7c2de2b
waimea-Jadavidson <168692078+waimea-Jadavidson@users.noreply.github.com>
2024-10-23 10:53:05 +1300

    [Commit] Dev

--- src/Main.kt
+++ src/Main.kt
@@ -18,67 +18,10 @@ import kotlin.random.Random
  * Program entry point
  */
 fun main() {
-    menu()
 
-    // initBoard(10, 3) // Menu entry point for game
 }
 
-fun menu(){
-    println("Welcome to Old Gold")
-    println("")
-    while (true){
-        println("Options:")
-        println("[1]. Play")
-        println("[2]. Exit")
-        print("Select: ")
-        val option = readln()
-        when(option){
-            "1" -> {
-                players()
-                println()
-                break
-            }
-            "2" -> break
-            else -> {
-                println("Invalid Option")
-                println()
-            }
-        }
-    }
-}
-
-fun players(){
-    println()
-    var numPlayers: Int
-    val playersList = arrayListOf<String>()
-    while (true) {
-        while (true) {
-            print("How many people are playing?: ")
-            numPlayers = readln().toInt()
-            if (numPlayers <=1){
-                println("Invalid Number of Players, please try again")
-            }
-            else{
-                break
-            }
-        }
-        println()
-        print("Are you sure? y/n: ")
-        if (readln().lowercase() == "y") {
-            println()
-            break
-        }
-    }
-    while (true) {
-        repeat(numPlayers) { i ->
-            print("What is player ${i + 1} name: ") // +1 required for Zero Index
-            playersList.add(i, readln().toString())
-            println()
-        }
-    }
-}
-
-fun initBoard(bSize: Int, bCoin: Int){  // Function takes in board size and number of coins
+fun initBoard(bSize: Int, bCoin: Int): MutableList<String>{  // Function takes in board size and number of coins
     val board = mutableListOf<String>()
     println(bSize-(bSize/bCoin))
     for(i in 1 .. bSize-(bSize/bCoin)){
@@ -91,4 +34,13 @@ fun initBoard(bSize: Int, bCoin: Int){  // Function takes in board size and numb
     board.add(0, "[G]")
     board.shuffle()
     println(board.joinToString(""))
+    return board
 }
+
+fun game(board: MutableList<String>){
+    println(board.joinToString(""))
+
+    println("Coin to move?: ")
+    val c1 = readln()
+}
+
