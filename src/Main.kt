54ca773100d84724ba6add02409e2e4ea0712a78
waimea-Jadavidson <168692078+waimea-Jadavidson@users.noreply.github.com>
2024-10-24 12:19:15 +1300

    [Commit] Push

--- src/Main.kt
+++ src/Main.kt
@@ -18,7 +18,7 @@ import kotlin.random.Random
  * Program entry point
  */
 fun main() {
-
+    game(initBoard(10, 2))
 }
 
 fun initBoard(bSize: Int, bCoin: Int): MutableList<String>{  // Function takes in board size and number of coins
@@ -33,14 +33,55 @@ fun initBoard(bSize: Int, bCoin: Int): MutableList<String>{  // Function takes i
     }
     board.add(0, "[G]")
     board.shuffle()
-    println(board.joinToString(""))
     return board
 }
 
+// This function is the Main Gameloop
 fun game(board: MutableList<String>){
-    println(board.joinToString(""))
+    while (true){
+        // Variable decleration for local use inside of function
+        var s1: Int
+        var s2: Int
 
-    println("Coin to move?: ")
-    val c1 = readln()
+        var count: Int
+
+        println("Player One") // Need to make this dynamic
+        println(board.joinToString(""))
+        println()
+        while(true){
+            print("Slot 1: ")
+            s1 = readln().toInt()
+
+            if(board[s1-1] == "[C]" || board[s1-1] == "[G]"){  // -1 added to deal with zero error
+                break
+            }
+            println(board[s1])
+            println("Invalid Move, Try Again!")
+        }
+
+        while(true) {
+            println()
+            print("Slot 2: ")
+            s2 = readln().toInt()
+
+            if (board[s2-1] == "[C]" || board[s2-1] == "[G]") { // -1 added to deal with zero error
+                break
+            }
+            println(board[s2])
+            println("Invalid Move, Try Again!")
+        }
+
+        for (i in s1-1 downTo s2-1){
+            if (board[i] == "[_]"){
+                count +=1
+            }
+            else{
+                print("Invalid Move, Try Again!")
+                break
+            }
+        }
+
+    }
+
 }
 
