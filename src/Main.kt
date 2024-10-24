a8de22f24665f4f876891b0781c53b2310963f5f
waimea-Jadavidson <168692078+waimea-Jadavidson@users.noreply.github.com>
2024-10-24 14:17:24 +1300

    [Commit] Fixed Main Loop
    Fixed bugs

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,3 @@
-import kotlin.random.Random
-
 /**
  * ------------------------------------------------------------------------
  * Mancala Rip-Off
@@ -17,15 +15,26 @@ import kotlin.random.Random
 /**
  * Program entry point
  */
+
+// Global Variables
+val SPACE = "[ ]"
+
+
 fun main() {
-    game(initBoard(10, 2))
+    val grid = initBoard(10,2) // Stores game board
+    while (true) {
+        displayBoard(grid)
+        if (playerMove(grid) == "WIN") break
+    }
+
+    println("Congrats Player {Insert Name Here}, you have won") // Need to do dynamic player count
 }
 
 fun initBoard(bSize: Int, bCoin: Int): MutableList<String>{  // Function takes in board size and number of coins
     val board = mutableListOf<String>()
     // println(bSize-(bSize/bCoin)) This was used for debug purposes
     for(i in 1 .. bSize-(bSize/bCoin)){
-        board.add("[_]")
+        board.add(SPACE)
     }
     // println(bSize/bCoin)
     for(i in 1..<(bSize/bCoin)){
@@ -36,53 +45,65 @@ fun initBoard(bSize: Int, bCoin: Int): MutableList<String>{  // Function takes i
     return board
 }
 
-// This function is the Main Gameloop
-fun game(board: MutableList<String>){
-    while (true){
+fun displayBoard(board: MutableList<String>){
+    val gameBoard = board
+    println(gameBoard.joinToString(""))
+    for (i in 0 .. board.size-1){
+      print(" ${i+1} ")
+    }
+    println()
+}
+
+fun playerMove(board: MutableList<String>): String {
         // Variable decleration for local use inside of function
         var s1: Int
         var s2: Int
 
-
-
         println("Player One") // Need to make this dynamic
-        println(board.joinToString(""))
         println()
         while(true){
-            print("Which coin to move: ")
+            print("Which coin to move/remove: ")
             s1 = readln().toInt()-1
 
             if(board[s1] == "[C]" || board[s1] == "[G]"){  // -1 added to deal with zero error
                 break
             }
-            println(board[s1])
             println("Invalid Move, Try Again!")
         }
 
-        while(true) {
-            println()
-            print("Move it to where: ")
-            s2 = readln().toInt()-1
+        // Are we removing a coin?
+        if(s1 == 0) {
+            // Yes, so check if it's gold
+            if(board[0] == "[G]") {
+                // It is, so we're done
+                return "WIN"
+            }
+            // Otherwise, just remove it
+            board[0] = SPACE
+        }
+        else {
+            // No, so we need to find out where to move iot to
+            while (true) {
+                println()
+                print("Move it to where: ")
+                s2 = readln().toInt() - 1
 
-            if (board[s2] == "[_]") { // -1 added to deal with zero error
-                var coinCount = 0
-                for (i in s1-1 downTo s2){
-                    if (board[i] != "[_]"){
-                        coinCount++
+                if (board[s2] == SPACE) {
+                    var coinCount = 0
+                    for (i in s1 - 1 downTo s2) {
+                        if (board[i] != SPACE) {
+                            coinCount++
+                        }
+                    }
+                    if (coinCount == 0) {
+                        break
                     }
                 }
-                if (coinCount == 0) {
-                    break
-                }
+                println("Invalid Move, Try Again!")
             }
-            println("Invalid Move, Try Again!")
+            board[s2] = board[s1]
+            board[s1] = SPACE
         }
-
-        board[s2] = board[s1]
-        board[s1] = "[_]"
-
-
-    }
-
+    return "Continue"
 }
 
